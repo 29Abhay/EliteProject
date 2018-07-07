a307957d020e143c27d20909b924326f19c78914
pranali19 <40880045+pranali19@users.noreply.github.com>
2018-07-07 15:00:05 +0530

    AddCourse.php
    This is the controller for course table

--- Controller/AddCourse.php
+++ Controller/AddCourse.php
@@ -1,1 +1,33 @@
 
+
+<?php 
+
+require '../model/AddCourseModel.php';
+$emp=new courseInfo();
+$data=$emp->addcourse();
+  if ($data)
+          {  
+            echo "succesfully inserted data";
+          $response=array("status"=>1,"status_message"=>"data inserted ");
+          
+          header("Content-Type: Application/json");
+echo json_encode($response);  
+
+          }
+         else
+         {
+            $response=array("status"=>0,"status_message"=>"error in inserting");
+             
+             header("Content-Type: Application/json");
+echo json_encode($response);  
+         }
+
+
+       
+
+ ?>
+
+
+
+
+
