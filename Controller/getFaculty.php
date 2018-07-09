3efa0c1cf082f5e7b213a08a23b2f7828efa52f9
vivekoswal8197 <vivekk08011997>
2018-07-09 14:23:53 +0700

    getFaculty

--- Controller/getFaculty.php
+++ Controller/getFaculty.php
@@ -1,1 +1,18 @@
+<?php 
+require '../model/addfaculty.php';
 
+
+$url=$_SERVER["REQUEST_URI"];
+$temp=explode('/',$url);
+$mob=end($temp);
+
+$fac=new FacultyInfo;
+$res=$fac->getfaculty($mob);
+
+header("Content-Type: Application/json");
+
+echo json_encode($res);
+
+
+
+ ?>
