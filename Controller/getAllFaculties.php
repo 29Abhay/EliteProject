f04c8b7ddab43794ae0f5363910c95e1f0665972
vivekoswal8197 <vivekk08011997>
2018-07-09 14:26:50 +0700

    getAllFaculties

--- Controller/getAllFaculties.php
+++ Controller/getAllFaculties.php
@@ -1,1 +1,17 @@
+<?php 
+require '../model/addfaculty.php';
 
+
+$url=$_SERVER["REQUEST_URI"];
+$temp=explode('/',$url);
+$attr=end($temp);
+if ($attr=='all') {
+	$fac=new FacultyInfo;
+	$res=$fac->getAllFaculties();
+
+header("Content-Type: Application/json");
+
+echo json_encode($res);
+}
+
+?>
