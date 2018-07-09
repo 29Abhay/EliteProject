635a7771a3e0c3a1d6205a58a828305a22b1e47e
pranali19 <40880045+pranali19@users.noreply.github.com>
2018-07-09 14:56:20 +0530

    Add files via upload

---
+++ Controller/getCourse.php
@@ -0,0 +1,33 @@
+
+
+<?php 
+
+require '../model/AddCourseModel.php';
+$emp=new courseInfo();
+$data=$emp->getcourse();
+  if ($data)
+          {  
+            
+         $response=array("status"=>1,"status_message"=>$data);
+          
+          header("Content-Type: Application/json");
+echo json_encode($response);  
+
+          }
+         else
+         {
+            $response=array("status"=>0,"status_message"=>"error in fetching data");
+             
+             header("Content-Type: Application/json");
+echo json_encode($response);  
+         }
+
+
+       
+
+ ?>
+
+
+
+
+
