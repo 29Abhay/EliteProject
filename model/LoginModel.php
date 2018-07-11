d199245ad57136b2ae7714df8d806b9ae7ef1fce
theuniverseboss <40853491+theuniverseboss@users.noreply.github.com>
2018-07-11 13:35:11 +0530

    login

---
+++ model/LoginModel.php
@@ -0,0 +1,54 @@
+<?php 
+session_start();
+class Database{
+	var $servername="localhost";
+	var $username="root";
+	var $password="";
+public function login(){
+try{
+
+$conn= new PDO("mysql:host=$this->servername;dbname=institute",$this->username,$this->password); 
+
+$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+$id=$_POST['id'];
+$password = $_POST['password'];
+$role=$_POST['role'];
+
+$query = "SELECT * FROM login 
+        WHERE id=:id
+       and  password=:password and role=:role";
+
+$stmt=$conn->prepare($query);
+
+ $stmt->bindParam(':id',$id);
+  $stmt->bindParam(':password',$password);	
+   $stmt->bindParam(':role',$role);		
+
+$result=$stmt->execute();
+var_dump($result);
+
+$count =$stmt->rowCount();
+
+if($count==1){
+    $_SESSION["id"]=$id;
+    $_SESSION["password"]=$password;
+    $response=array("status" =>1 ,"status_message" =>"login successfully");
+}
+else{
+    $response=array("status" =>0 ,"status_message" =>"invalid details");
+}
+}
+catch(PDOException $ex){
+			echo "Connection failed". $ex->getMessage();
+		}
+	
+	
+header('Content-Type: application/json');
+echo json_encode($response);
+}}
+?>
+
+
+
+
