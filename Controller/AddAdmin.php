5ed75185a754738ad94bf4dde73811b7fa2f855f
abhi11111 <37204429+abhi11111@users.noreply.github.com>
2018-07-12 09:34:19 +0530

    Admin single class

--- Controller/AddAdmin.php
+++ Controller/AddAdmin.php
@@ -1,7 +1,8 @@
 <?php 
 require '../Model/AddAdminModel.php';
 
-$obj2= new AdminInfo;
+$obj2= new Admin;
 $a=$obj2->addadmin();
+header('Content-Type: application/json');
 echo json_encode($a);
  ?>
--- model/AddAdminModel.php
+++ model/AddAdminModel.php
@@ -1,11 +1,20 @@
 <?php 
 require 'Database/database.php';
+
 class Admin{
 private $mob;
 private $password;
 private $fname;
 private $lname;
 private $aadhar_no;
+public $conn;
+public function __construct()
+  
+  {
+    
+    $obj1=new Database;
+     $this->conn=$obj1->getconnection();
+    }
 
 public function setmob($mob){
 $this->mob=$mob;
@@ -37,41 +46,27 @@ return $this->lname;
 public function getaadhar_no(){
 return $this->aadhar_no;
 }
-   }
-class AdminInfo{
-	var $admin;
-	var $conn;
-	
-
-	function __construct()
-	
-	{
-		$this->admin=new Admin;
-		$obj1=new Database;
-			$this->conn=$obj1->getconnection();
-    }
-
 public function addadmin(){
 try{
-			
+      
 
-		$qry="insert into admin(mob,password,fname,lname,aadhar_no) values(:mob,:password,:fname,:lname,:aadhar_no)";
+    $qry="insert into admin(mob,password,fname,lname,aadhar_no) values(:mob,:password,:fname,:lname,:aadhar_no)";
 
-  if (isset($_POST['mob'])){$this->admin->setmob(htmlspecialchars($_POST["mob"]));}
-  if (isset($_POST['password'])){$this->admin->setpassword(htmlspecialchars($_POST["password"]));}
-  if (isset($_POST['fname'])){$this->admin->setfname(htmlspecialchars($_POST["fname"]));}
-  if (isset($_POST['lname'])){$this->admin->setlname(htmlspecialchars($_POST["lname"]));}
-  if (isset($_POST['aadhar_no'])){$this->admin->setaadhar_no(htmlspecialchars($_POST["aadhar_no"]));}
+  if (isset($_POST['mob'])){$this->setmob(htmlspecialchars($_POST["mob"]));}
+  if (isset($_POST['password'])){$this->setpassword(htmlspecialchars($_POST["password"]));}
+  if (isset($_POST['fname'])){$this->setfname(htmlspecialchars($_POST["fname"]));}
+  if (isset($_POST['lname'])){$this->setlname(htmlspecialchars($_POST["lname"]));}
+  if (isset($_POST['aadhar_no'])){$this->setaadhar_no(htmlspecialchars($_POST["aadhar_no"]));}
 
-     $stmt=$this->conn->prepare($qry);
+     $stmt= $this->conn->prepare($qry);
 
-     $mob=$this->admin->getmob();
-     $password=$this->admin->getpassword();
-     $fname=$this->admin->getfname();
-     $lname=$this->admin->getlname();
-     $aadhar_no=$this->admin->getaadhar_no();	
+     $mob=$this->getmob();
+     $password=$this->getpassword();
+     $fname=$this->getfname();
+     $lname=$this->getlname();
+     $aadhar_no=$this->getaadhar_no(); 
 
-     $stmt->bindParam(':mob',$mob);	
+     $stmt->bindParam(':mob',$mob); 
      $stmt->bindParam(':password',$password);
      $stmt->bindParam(':fname',$fname);
      $stmt->bindParam(':lname',$lname);
@@ -81,21 +76,21 @@ try{
   
      if($result)
 {
-	$response=array("status" =>1 ,"status_message" =>"data inserted");
+  $response=array("status" =>1 ,"status_message" =>"data inserted");
 }
 else
 {
-$response=array("status" =>0 ,"status_message" =>"error in inserting");	
-		}
-	}
-		catch(PDOException $ex){
-			echo $ex->getMessage() ;
-	if($ex->getcode()==23000){
-		$response=array("status" =>0 ,"status_message" =>"duplicate entry for mob");
-	}
+$response=array("status" =>0 ,"status_message" =>"error in inserting"); 
+    }
+  }
+    catch(PDOException $ex){
+      echo $ex->getMessage() ;
+  if($ex->getcode()==23000){
+    $response=array("status" =>0 ,"status_message" =>"duplicate entry for mob");
+  }
 }
-header('Content-Type: application/json');
-echo json_encode($response);   
+ return $response;
    }
-      }
+   }
+
 ?>
