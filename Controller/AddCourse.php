f0c61be025fd8b264036e9c3601bf7157a41aed3
pranali19 <40880045+pranali19@users.noreply.github.com>
2018-07-12 12:02:21 +0530

    Update AddCourse.php

--- Controller/AddCourse.php
+++ Controller/AddCourse.php
@@ -1,31 +1,65 @@
-
-
 <?php 
+session_start();
 
 require '../model/AddCourseModel.php';
+
+if (isset($_SESSION['usernameSession']) && isset($_SESSION['passwordSession'])){
+
+
+
+         $response=array("status"=>1,"status_message"=>"valid");
+          
+
+
+           
+             
+          
 $emp=new courseInfo();
 $data=$emp->addcourse();
   if ($data)
           {  
-            echo "succesfully inserted data";
-          $response=array("status"=>1,"status_message"=>"data inserted ");
-          
-          header("Content-Type: Application/json");
-echo json_encode($response);  
+            
+         $response=array("status"=>1,"status_message"=>"data inserted");
+           
 
           }
          else
          {
-            $response=array("status"=>0,"status_message"=>"error in inserting");
+            $response=array("status"=>0,"status_message"=>"error fetching data");
              
-             header("Content-Type: Application/json");
-echo json_encode($response);  
+          
+       
+            
          }
 
+  
 
+
+}  else 
+{
+ 
+               $response=array("status"=>0,"status_message"=>"invalid operation");
+            
+
+}
+
+                  header("Content-Type: Application/json");
+                 echo json_encode($response);
+                
+
+
+ 
        
 
  ?>
+
+
+
+
+
+
+
+
 
 
 
