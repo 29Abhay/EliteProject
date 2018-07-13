9c9f792532f1e3cf0c2d6a70ee3768ff2cfc8c3a
theuniverseboss <40853491+theuniverseboss@users.noreply.github.com>
2018-07-13 10:53:10 +0530

    Add admin controller

--- Controller/AddAdmin.php
+++ Controller/AddAdmin.php
@@ -1,8 +1,8 @@
 <?php 
-require '../Model/AddAdminModel.php';
+require '../Model/AdminModel.php';
 
-$obj2= new Admin;
-$a=$obj2->addadmin();
+$obj2= new AdminInfo;
+$res=$obj2->addadmin();
 header('Content-Type: application/json');
-echo json_encode($a);
+echo json_encode($res); 
  ?>
