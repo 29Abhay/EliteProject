9fcb4a30adb24ea4a82d0d74d7c1578dc9a6c075
shubh-ng <40854895+shubh-ng@users.noreply.github.com>
2018-07-13 20:01:43 +0530

    job component used in enquiry component

---
+++ view/src/app/job/job.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-job',
+  templateUrl: './job.component.html',
+  styleUrls: ['./job.component.css']
+})
+export class JobComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { 
+  }
+
+
+  public courses=[
+      {'name':'--Select Courses--' },
+      {'name':'C++' },
+      {'name':'JAVA' },
+      {'name':'PHP' },
+      {'name':'ANGULAR' },
+      {'name':'TALLY' },
+      {'name':'DCA' },
+  ];
+
+}
