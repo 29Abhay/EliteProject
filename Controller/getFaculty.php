0562927343e3a537cf05b3c260ca96ab2ec6cb30
jainhrithik <hrithikjain042@gmail.com>
2018-07-13 10:43:47 +0530

    get faculty by mobile no

--- Controller/getFaculty.php
+++ Controller/getFaculty.php
@@ -1,17 +1,37 @@
 <?php 
-require '../model/addfaculty.php';
+require '../model/addFacultyModel.php';
 
 
-$url=$_SERVER["REQUEST_URI"];
-$temp=explode('/',$url);
-$mob=end($temp);
+ //$url=$_SERVER["REQUEST_URI"];
+ //$temp=explode('/',$url);$mob=end($temp);
+$response=array();
+try{
+	if(!empty($_POST["mobile_no"]))
+	{
+	$fac=new FacultyInfo;
+	$res=$fac->getfaculty($_POST["mobile_no"]);
+     if ($res) {
+     	$response=$res;
+     }
+     else{
+     	$response=array("status"=>0,"status_message"=>"entry not found ");
+     }
+	
+	}
+	else
+	{
+	$response=array("status"=>0,"status_message"=>"enter the mobile no ");
+	}
+ }
+catch(PDOException $ex)
+{
+echo "exception occused "." ".$ex,getmessage();
 
-$fac=new FacultyInfo;
-$res=$fac->getfaculty($mob);
+}
 
 header("Content-Type: Application/json");
 
-echo json_encode($res);
+echo json_encode($response);
 
 
 
