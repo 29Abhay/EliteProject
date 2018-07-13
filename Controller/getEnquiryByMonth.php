a5239d53a32e025973baa25902210b17c2d6e09e
SACHIN SOLANKI <40853494+sachinkillerman@users.noreply.github.com>
2018-07-13 18:32:01 +0530

    Update getEnquiryByMonth.php

--- Controller/getEnquiryByMonth.php
+++ Controller/getEnquiryByMonth.php
@@ -1,16 +1,15 @@
 <?php 
-//session_start();
-//if (isset($_SESSION["username_session"]) && isset($_SESSION["password_session"])) 
-//{
+session_start();
+if (isset($_SESSION["username_session"]) && isset($_SESSION["password_session"])) 
+{
    require '../Model/EnquiryModel.php';
     $enq=new EnquiryInfo();
     $res=$enq->getEnquiryByMonth();
-
-//}
-	//else
-//{
-	//$res=array("status"=>0,"status_message"=>"invalid operation");
-//}
+}
+else
+{
+	$res=array("status"=>0,"status_message"=>"invalid operation");
+}
 
 header("Content-Type: Application/json");
 echo json_encode($res);
