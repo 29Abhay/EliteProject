ad7fe145afd48d826f3515bb617557d92476d830
theuniverseboss <40853491+theuniverseboss@users.noreply.github.com>
2018-07-13 12:52:41 +0530

    admin controller

--- Controller/AddAdmin.php
+++ Controller/AddAdmin.php
@@ -1,8 +1,14 @@
-<?php 
+<?php
+session_start();
+if(isset($_SESSION['usernameSession']) and isset($_SESSION['passwordSession'])){ 
 require '../Model/AdminModel.php';
-
 $obj2= new AdminInfo;
 $res=$obj2->addadmin();
+}
+else
+{
+    $response=array("status" =>0 ,"status_message" =>"invalid operation");
+}
 header('Content-Type: application/json');
 echo json_encode($res); 
  ?>
--- Controller/AddOccupation.php
+++
@@ -1,9 +0,0 @@
-<?php 
-require '../Model/occupationModel.php';
-
-$obj2= new OccupationInfo;
-$res=$obj2->addoccupation();
-header('Content-Type: application/json');
-echo json_encode($res);
-
- ?>
