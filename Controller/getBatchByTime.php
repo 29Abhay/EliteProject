72e7e76583fa492d2062a6f7def9b68eb9180dce
pranali19 <40880045+pranali19@users.noreply.github.com>
2018-07-14 09:39:07 +0530

    Update getBatchByTime.php

--- Controller/getBatchByTime.php
+++ Controller/getBatchByTime.php
@@ -1,58 +1,39 @@
+ 
 <?php 
-session_start();
-
-
-
-if (isset($_SESSION['usernameSession']) && isset($_SESSION['passwordSession'])){
-
-require '../model/AddBatchModel.php';
-
-
-         $response=array("status"=>1,"status_message"=>"valid");
-          
-
-
-           
-             
-          
+ session_start(); 
+        
+if (isset($_SESSION['usernameSession']) && isset($_SESSION['passwordSession'])){ 
+require '../model/BatchModel.php';       
 $emp=new batchInfo();
-$data=$emp->getbatch_time();
-  if ($data)
-          {  
-            
-         $response=array("status"=>1,"status_message"=>$data);
-           
-
-          }
-         else
-         {
-            $response=array("status"=>0,"status_message"=>"error fetching data");
-             
-          
-       
-            
-         }
-
-  
-
-
-}  else 
-{
- 
-               $response=array("status"=>0,"status_message"=>"invalid operation");
-            
-
+$response=$emp->getbatch_time();
 }
-
-                  header("Content-Type: Application/json");
-                 echo json_encode($response);
-                
-
-
- 
-       
-
+else{
+      $response=array("status"=>0,"status_message"=>"error");
+}
+header("Content-Type: Application/json");
+echo json_encode($response);
+               
  ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
