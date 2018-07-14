10b6b6a87c1e65711e4f46d935016a172a3ae5aa
pranali19 <40880045+pranali19@users.noreply.github.com>
2018-07-14 10:11:18 +0530

    Update addCourse.php

--- Controller/addCourse.php
+++ Controller/addCourse.php
@@ -1,57 +1,33 @@
 <?php 
-session_start();
+ session_start(); 
+        
+if (isset($_SESSION['usernameSession']) && isset($_SESSION['passwordSession'])){ 
 
 
-
-if (isset($_SESSION['usernameSession']) && isset($_SESSION['passwordSession'])){
-
-require '../model/AddCourseModel.php';
-
-         $response=array("status"=>1,"status_message"=>"valid");
-          
-
-
-           
-             
-          
+require '../model/CourseModel.php';          
 $emp=new courseInfo();
-$data=$emp->addcourse();
-  if ($data)
-          {  
-            
-         $response=array("status"=>1,"status_message"=>"data inserted");
-           
-
-          }
-         else
-         {
-            $response=array("status"=>0,"status_message"=>"error fetching data");
-             
-          
-       
-            
-         }
-
-  
-
-
-}  else 
-{
- 
-               $response=array("status"=>0,"status_message"=>"invalid operation");
-            
-
+$response=$emp->addcourse();
 }
-
-                  header("Content-Type: Application/json");
-                 echo json_encode($response);
-                
-
-
- 
-       
-
+else{
+      $response=array("status"=>0,"status_message"=>"error");
+}
+header("Content-Type: Application/json");
+echo json_encode($response);
+               
  ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
